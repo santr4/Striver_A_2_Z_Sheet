cdf960ce9b65a1b650261a881e782ec7bea94f75
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-09-15 14:37:07 +0530

    diamond pattern

---
+++ Step1_Basics/Lec2/tut1.cpp
@@ -0,0 +1,53 @@
+// ** Diamond Pattern **
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void erect_diamond_pattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void invert_diamond_pattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    erect_diamond_pattern(n);
+    invert_diamond_pattern(n);
+    return 0;
+}
