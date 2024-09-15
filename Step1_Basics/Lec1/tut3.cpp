b8a836124601c471bbc1ccb81af3550388a50383
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-09-15 18:29:06 +0530

    if-else decision making

---
+++ Step1_Basics/Lec1/tut3.cpp
@@ -0,0 +1,22 @@
+// ** if-else statements
+
+class Solution
+{
+public:
+    string compareNM(int n, int m)
+    {
+        // code here
+        if (n < m)
+        {
+            return "lesser";
+        }
+        else if (n == m)
+        {
+            return "equal";
+        }
+        else
+        {
+            return "greater";
+        }
+    }
+};
