9794f1a288ba3570989f3a50918a55dbde73f978
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-09-18 14:22:10 +0530

    sum of all divisors from 1 to n

--- Step1_Basics/Lec4/tut6.cpp
+++ Step1_Basics/Lec4/tut6.cpp
@@ -1,5 +1,8 @@
 // sum of all divisors from 1 to n
 
+// ** intution: here the sum of divisors of all the numbers from 1 to n is calculated and returned. So for a divisor i there can be multiple numbers for which
+// ** i is a divisor. So we will calculate how many multiples of i are there from 1 to n and then multiply it with i to get the sum of divisors of i.
+
 class Solution
 {
 public:
